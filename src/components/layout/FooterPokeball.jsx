8b5085e22a1282f4b335ddcb1571815b83185b23
Juan A. Cárcamo <juancatu25@gmail.com>
2023-08-25 17:53:50 -0500

    le di estilos al home y de di la funcionalidad del prevent defaul al imput

--- src/components/layout/FooterPokeball.jsx
+++ src/components/layout/FooterPokeball.jsx
@@ -1,13 +1,17 @@
 const FooterPokeball = () => {
     return (
-        <section>
-            <div className="h-16 bg-red-700"></div>
-            <div className="h-12 bg-black relative">
-                <div className="h-14 aspect-square bg-white rounded-full absolute 
-                left-1/2 -translate-x-1/2 -top-6 border-[8px] border-black
-                after:block after:content-[''] after:h-8 after:aspect-square after:bg-green-500 after:rounded-full"></div>
-            </div>
-        </section>
-    )
+      <section>
+        <div className="h-16 bg-red-700"></div>
+        <div className="h-12 bg-black relative">
+          <div
+            className="h-14 aspect-square bg-white rounded-full absolute 
+                left-1/2 -translate-x-1/2 -top-8 border-[8px] border-black
+                after:block after:content-[''] after:h-8 after:aspect-square after:bg-slate-700
+                 after:rounded-full after:absolute after:left-1/2 
+                 after:-translate-x-1/2 after:top-1/2 after:-translate-y-1/2 after:border-[5px] after:border-black"
+          ></div>
+        </div>
+      </section>
+    );
 }
 export default FooterPokeball
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,51 @@
 import FooterPokeball from "../components/layout/FooterPokeball"
 
 const Home = () => {
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const inputValue = e.target.name.value.trim();
+
+      if (inputValue.length !== 0) {
+        dispatch(setNameTrainer(inputValue));
+        navigate("/pokedex");
+      }
+      e.target.name.value = "";
+    };
+
     return (
-        <main className="min-h-screen grid grid-rows-[1fr_auto]">
-            <section>
-                <article>
-                    <div>
-                        <img src="/images/banner.png" alt="" />
-                    </div>
-                    <h2>Hi, Trainer!</h2>
-                    <p>To start, give me your name</p>
-                    <form>
-                        <input type="text" />
-                        <button>Start</button>
-                    </form>
-                </article>
-            </section>
-            <FooterPokeball />
-        </main>
-    )
+      <main className="min-h-screen grid grid-rows-[1fr_auto]">
+        <section className="">
+          <article className="gap-[2rem] h-[80%] flex flex-col justify-center items-center">
+            <div className="">
+              <img
+                className=" sm:w-[400px] md:w-[600px] lg:w-[700px] w-[300px]"
+                src="/images/banner.png"
+                alt=""
+              />
+            </div>
+            <div className="text-center flex flex-col">
+              <h2 className=" lg:text-4xl font-bold  text-[#FE1936] text-2xl">
+                ¡Hello Trainer!
+              </h2>
+              <p>To start, give me your name</p>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <input
+                className="shadow-md py-[8px] px-2"
+                autoComplete="off"
+                placeholder="Enter your name..."
+                id="nameTrainer"
+                type="text"
+                required
+              />
+              <button className="bg-[#D93F3F] py-[8px] w-[100px] text-white">
+                Start
+              </button>
+            </form>
+          </article>
+        </section>
+        <FooterPokeball />
+      </main>
+    );
 }
 export default Home
