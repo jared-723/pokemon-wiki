94e23f14ec70b7970160256177801689ae87f01a
jared-723 <jarwend723@gmail.com>
2023-08-25 02:27:37 +0000

    finished footer Pokeball

--- src/components/layout/FooterPokeball.jsx
+++ src/components/layout/FooterPokeball.jsx
@@ -3,9 +3,11 @@ const FooterPokeball = () => {
         <section>
             <div className="h-16 bg-red-700"></div>
             <div className="h-12 bg-black relative">
-                <div className="h-14 aspect-square bg-white rounded-full absolute 
+                <div className="h-16 aspect-square bg-white rounded-full absolute 
                 left-1/2 -translate-x-1/2 -top-6 border-[8px] border-black
-                after:block after:content-[''] after:h-8 after:aspect-square after:bg-green-500 after:rounded-full"></div>
+                after:block after:content-[''] after:h-8 after:aspect-square after:bg-slate-800 
+                after:rounded-full after:absolute after:left-1/2 after:-translate-x-1/2 after:top-1/2 after:-translate-y-1/2 
+                after:border-[6px] after:border-black"></div>
             </div>
         </section>
     )
