29242ca3821ffb9aeeca4e138cd46aa80a7f0342
jared-723 <jarwend723@gmail.com>
2023-08-27 05:20:59 +0000

    arreglando pokemonDetail

--- src/components/pokemonDetail/StatBarList.jsx
+++ src/components/pokemonDetail/StatBarList.jsx
@@ -1,15 +1,18 @@
-import BarProgressStat from "./BarProgressStat"
+import BarProgressStat from "./BarProgressStat";
 
-const StatBarList = ({stats}) => {
+const StatBarList = ({ stats }) => {
   return (
     <section>
-        <h2>Stats</h2>
-        <section>
-            {
-                stats?.map((stat) => <BarProgressStat key={stat.name} stat={stat}/>)
-            }
-        </section>
+      <section className="flex items-center gap-2">
+        <h2 className="font-bold">Stats</h2>
+        <hr className="w-full"/>
+      </section>
+      <section>
+        {stats?.map((stat) => (
+          <BarProgressStat key={stat.name} stat={stat} />
+        ))}
+      </section>
     </section>
-  )
-}
-export default StatBarList
+  );
+};
+export default StatBarList;
