762e4f4dd076c93f58c48ec2f45039af24fe297c
SimonGuerra007 <guerrasimon007@gmail.com>
2023-08-29 05:36:32 -0500

    error de la paginación arreglado

--- src/utils/pagination.js
+++ src/utils/pagination.js
@@ -12,7 +12,7 @@ const paginateData = (items, currentPage) =>{
     const lastPage = Math.ceil(items.length / ITEMS_PER_PAGE)
 
     //Bloque actual
-    const PAGES_PER_BLOCK = 5;
+    const PAGES_PER_BLOCK = 7;
     const actualBlock = Math.ceil(currentPage / PAGES_PER_BLOCK)
 
     //Paginas que se van a mostrar en el bloque actual
