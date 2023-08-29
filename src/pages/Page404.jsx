e2085c30fe0ba9e7b3b4e5823be9ca68ac8cf23f
Juan A. Cárcamo <juancatu25@gmail.com>
2023-08-28 23:48:27 -0500

    pagina error 404, estilos con css y tambien con tailwind

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,8 +1,14 @@
 const Page404 = () => {
-    return (
-      <div>
-        <img src="/public/images/pikachu.gif" alt="pikachu error 404" />
-      </div>
-    );
+  return (
+    <main className="bg-[#e8ebf1] h-screen xl:flex-row sm:px-2 flex flex-col justify-center items-center">
+      <h1 className="text-center text-[60px] font-bold font-serif">
+        Error 404
+      </h1>
+      <p className="text-semibold text-[30px] text-center font-serif px-[5px]">
+        Oops! We're sorry, but the page you're trying to find doesn't exist.
+      </p>
+      <div className="page404Bg"></div>
+    </main>
+  );
 }
 export default Page404
