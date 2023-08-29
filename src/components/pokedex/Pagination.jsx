71cdc53b86c3d39591e5d3e05c79a8ee21c96b18
SimonGuerra007 <guerrasimon007@gmail.com>
2023-08-29 06:00:54 -0500

    error en responsive arreglado

--- src/components/pokedex/Pagination.jsx
+++ src/components/pokedex/Pagination.jsx
@@ -22,16 +22,16 @@ const Pagination = ({ lastPage, pagesInCurrentBlock, setCurrentPage, currentPage
   const handleFirstPage = () => setCurrentPage(FIRST_PAGE);
 
   return (
-    <ul className="flex justify-center gap-4 p-4 items-center mb-[50px]">
-      {currentPage >=2 && <li onClick={handleFirstPage} className={`cursor-pointer py-[11px] px-[11px] font-bold rounded-[3px] text-xs transition-all ${darkmode ? 'bg-[#ffc125] hover:bg-[#ffd56b]' : 'bg-[#ff0000] hover:bg-[#ff5b5b]'}`}>{"<<"}</li>}
-      {currentPage >=2 && <li onClick={handlePreviousPage} className={`cursor-pointer py-[11px] px-4 font-bold rounded-[3px] text-xs transition-all ${darkmode ? 'bg-[#ffc125] hover:bg-[#ffd56b]' : 'bg-[#ff0000] hover:bg-[#ff5b5b]'}`}>{"<"}</li>}
+    <ul className="flex justify-center justify-self-center gap-4 p-4 items-center mb-[50px] w-[80%]">
+      {currentPage >=2 && <li onClick={handleFirstPage} className={`cursor-pointer py-2 px-1 font-bold rounded-[3px] text-xs transition-all ${darkmode ? 'bg-[#ffc125] hover:bg-[#ffd56b]' : 'bg-[#ff0000] hover:bg-[#ff5b5b]'}`}>{"<<"}</li>}
+      {currentPage >=2 && <li onClick={handlePreviousPage} className={`cursor-pointer py-2 px-2 font-bold rounded-[3px] text-xs transition-all ${darkmode ? 'bg-[#ffc125] hover:bg-[#ffd56b]' : 'bg-[#ff0000] hover:bg-[#ff5b5b]'}`}>{"<"}</li>}
       {pagesInCurrentBlock.map((page) => (
-        <li  className={`py-2 px-4 cursor-pointer rounded-[3px] transition-all ${currentPage === page ? "text-white bg-[#ff0000]" : "hover:bg-red-400"} ${darkmode ? 'text-white' : 'text-black'}`} key={page} onClick={() => setCurrentPage(page)}>
+        <li  className={`py-1 px-3 cursor-pointer rounded-[3px] transition-all ${currentPage === page ? "text-white bg-[#ff0000]" : "hover:bg-red-400"} ${darkmode ? 'text-white' : 'text-black'}`} key={page} onClick={() => setCurrentPage(page)}>
           {page}
         </li>
       ))}
-      <li onClick={handleNextPage} className={`cursor-pointer py-[11px] px-4 font-bold rounded-[3px] text-xs transition-all ${darkmode ? 'bg-[#ffc125] hover:bg-[#ffd56b]' : 'bg-[#ff0000] hover:bg-[#ff5b5b]'}`}>{">"}</li>
-      <li onClick={handleLastPage} className={`cursor-pointer py-[11px] px-[11px] font-bold rounded-[3px] text-xs transition-all ${darkmode ? 'bg-[#ffc125] hover:bg-[#ffd56b]' : 'bg-[#ff0000] hover:bg-[#ff5b5b]'}`}>{">>"}</li>
+      <li onClick={handleNextPage} className={`cursor-pointer py-2 px-2 font-bold rounded-[3px] text-xs transition-all ${darkmode ? 'bg-[#ffc125] hover:bg-[#ffd56b]' : 'bg-[#ff0000] hover:bg-[#ff5b5b]'}`}>{">"}</li>
+      <li onClick={handleLastPage} className={`cursor-pointer py-2 px-1 font-bold rounded-[3px] text-xs transition-all ${darkmode ? 'bg-[#ffc125] hover:bg-[#ffd56b]' : 'bg-[#ff0000] hover:bg-[#ff5b5b]'}`}>{">>"}</li>
     </ul>
   );
 };
--- src/utils/pagination.js
+++ src/utils/pagination.js
@@ -12,7 +12,7 @@ const paginateData = (items, currentPage) =>{
     const lastPage = Math.ceil(items.length / ITEMS_PER_PAGE)
 
     //Bloque actual
-    const PAGES_PER_BLOCK = 7;
+    const PAGES_PER_BLOCK = 4;
     const actualBlock = Math.ceil(currentPage / PAGES_PER_BLOCK)
 
     //Paginas que se van a mostrar en el bloque actual
